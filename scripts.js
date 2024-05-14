3e7019e3c2274bb104071afadaefe22bd0af10bc
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-14 10:58:59 +0200

    Refactored to an OOP approach

--- scripts.js
+++ scripts.js
@@ -6,9 +6,29 @@ import { state, Task } from "./state.js";
 /**
  * 
  * @param {string} dataAttr 
- * @param {string} [value] 
+ * @param {string} [value]
+ * @returns {HTMLElement}
  */
 const getHtml = (dataAttr, value) => {
+    const selector = value ? `data-${dataAttr}="${value}"` : `data-${dataAttr}`;
+    const element = document.querySelector(selector);
+    const isHtmlElement = element instanceof HTMLElement;
+
+    if (!isHtmlElement) {
+        throw new Error('${selector} attribute not found in HTML');
+    }
+
+    return element;
+};
+
+
+/**
+ * 
+ * @param {string} dataAttr 
+ * @param {string} [value]
+ * @returns {boolean}
+ */
+const doesHtmlExist = (dataAttr, value) => {
     const selector = value ? `data-${dataAttr}="${value}"` : `data-${dataAttr}`;
     const element = document.querySelector(selector);
     const isHtmlElement = element instanceof HTMLElement;
